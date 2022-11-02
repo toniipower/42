d5f55980f73acc72617f6ec36e4ef0c63b31c21b
toniipower <antoniohl90@gmail.com>
2022-11-02 20:04:46 +0100

    first commit

---
+++ c_shell00/ft_print_alphabet.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aheredia <aheredia@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 13:48:50 by aheredia          #+#    #+#             */
+/*   Updated: 2022/02/11 14:13:04 by aheredia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+	char a='a';
+	
+	while(a<='z')
+}
+	write("%3a",a);
+	a++;
+}
+}
+
+int main()
+{
+	void ft_print_alphabet(void);
+	return (0);
+}
